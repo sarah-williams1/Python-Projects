50ead3e7b28f53fdcc5fef980c300f0930cd2e9d
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-09-26 15:12:35 -0400

    Add files via upload
    Variables and data types assignment

---
+++ variables_and_datatypes.py
@@ -0,0 +1,29 @@
+"""
+Variables and Datatypes.py
+Author: Sarah Williams
+Created: 27 August 2024
+
+"""
+
+integerVariable = 300
+otherInteger = 4
+floatVariable = 321.0987
+otherFloat = 9.53
+stringVariable = "Python is confusing because I don't have to explicitly declare data types...this is going to be the death of me."
+otherString = "This is a string"
+booleanVariable = True
+otherBoolean = floatVariable < integerVariable
+
+print(integerVariable )
+print(otherInteger )
+
+print(floatVariable )
+print(otherFloat)
+
+print(stringVariable)
+print(otherString)
+
+print(booleanVariable)
+print(otherBoolean)
+
+
